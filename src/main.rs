05f6444deff9bea22d289a2e9ffcf1f072b30304
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-09-12 15:43:13 +0200

    Added a basic discord bot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-use serenity::*;
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use dotenv::dotenv;
+
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+#[tokio::main]
+async fn main() {
+    dotenv();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::builder(&token).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
