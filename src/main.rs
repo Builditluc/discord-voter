d0c742427acd6dc36cd870f97b2033f6a29ad2be
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-09-20 21:30:09 +0200

    Added the start_vote command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,95 @@
 use dotenv::dotenv;
-use serenity::{
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
-};
+use serenity::framework::standard::CommandResult;
+use serenity::prelude::TypeMapKey;
 use std::env;
-mod eventhandler;
+use std::default::Default;
+
+use serenity::client::{Client, Context};
+use serenity::framework::standard::{
+    macros::{command, group},
+    Args, StandardFramework,
+};
+use serenity::model::channel::Message;
+
+// TODO: move this into vote_data.rs
+struct CurrentVote {
+    pub title: String,
+    pub description: String,
+    running: bool,
+}
+
+impl Default for CurrentVote {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl CurrentVote {
+    pub fn new() -> Self {
+        CurrentVote {
+            title: String::new(),
+            description: String::new(),
+            running: false,
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.running
+    }
+}
+
+impl TypeMapKey for CurrentVote {
+    type Value = CurrentVote;
+}
+
+#[group]
+#[commands(start_vote)]
+struct General;
+
+// TODO: move this into admin_commands.rs
+#[command]
+async fn start_vote(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+
+    // first, check if there is already a vote taking place
+    if !data.contains_key::<CurrentVote>() { msg.reply(ctx, "No instance of CurrentVote was found inside of the client data").await; return Ok(())}
+    let vote = data.get::<CurrentVote>().unwrap();
+    if !vote.is_running() { msg.reply(ctx, "There is a vote already taking place").await; return Ok(())}
+
+    // create a new vote with the given title and description
+    let mut new_vote: CurrentVote = Default::default();
+    
+    new_vote.title = args.single::<String>().unwrap_or(String::new());
+    new_vote.description = args.single::<String>().unwrap_or(String::new());
+
+    {
+        let mut data = ctx.data.write().await;
+        data.insert::<CurrentVote>(new_vote);
+    }
+
+    msg.channel_id.say(ctx, "Created a new Vote").await;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-    dotenv();
+    if let Err(error) = dotenv() {
+        panic!("Error loading the .env file: {:?}", error);
+    };
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    let mut client = Client::builder(&token)
-        .event_handler(eventhandler::Handler)
-        .await
-        .expect("Err creating client");
+    // create the framework and the client
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("#"))
+        .group(&GENERAL_GROUP);
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+    let mut client = Client::builder(&token)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(error) = client.start().await {
+        println!("Client error: {:?}", error);
     }
 }
